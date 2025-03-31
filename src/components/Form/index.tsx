bd0a46f0f11c4458976ce182bc135a9c6c2de999
davanso <guipvp234@gmail.com>
2025-03-31 20:28:47 -0300

    feat ✨: add controller to the form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,20 +3,47 @@ import Button from '../Button';
 import style from './form.module.scss';
 
 class Form extends React.Component {
+    state = {
+        task: '',
+        time: '00:00'
+    }
+
+    addTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('state: ', this.state);
+    }
+
     render() {
         return (
-            <form className={style.newTask}>
+            <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="task">
                         Add a new study theme
                     </label>
-                    <input type="text" name="task" id="task" placeholder="What do you want to study?" required />
+                    <input
+                        type="text"
+                        name="task"
+                        id="task"
+                        value={this.state.task}
+                        onChange={event => this.setState({...this.state, task: event.target.value})}
+                        placeholder="What do you want to study?"
+                        required
+                    />
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="time">
                         Time
                     </label>
-                    <input type="time" name="time" id="time" step="1" min="00:00:00" max="01:30:00" required />
+                    <input
+                        type="time"
+                        name="time"
+                        id="time"
+                        value={this.state.time}
+                        onChange={event => this.setState({...this.state, time: event.target.value})}
+                        step="1"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required />
                 </div>
                 <Button>
                     Add
