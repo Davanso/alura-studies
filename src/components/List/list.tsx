6c41c45eb432971c2d1293f280e53405db3cc173
davanso <guipvp234@gmail.com>
2025-03-31 20:10:14 -0300

    feat ✨: add list state

--- src/components/List/list.tsx
+++ src/components/List/list.tsx
@@ -1,18 +1,21 @@
 import style from './list.module.scss'
 import ItemList from "./item/item";
+import {useState} from "react";
 
 function List() {
-    const tasks = [{
+    const  [tasks, setTasks] = useState([{
         task: 'React',
         time: '02:00:00',
     }, {
         task: 'Javascript',
         time: '01:00:00',
-    }]
+    }])
 
     return (
         <aside className= {style.taskList}>
-            <h2>Studies of the day</h2>
+            <h2 onClick={() =>
+                setTasks([...tasks, {task: 'React', time: '02:00:00'}])
+            }>Studies of the day</h2>
             <ul>
                 {tasks.map((task, index) => (
                     <ItemList
